b3b5ba5852cbcbd674d0ba75a1ce3871fc6a39db
Iamdodo22 <asimiazeez493@gail.com>
2023-10-19 23:09:13 +0100

    progress ...

--- script.js
+++ script.js
@@ -17,11 +17,17 @@ const cardCvv = document.querySelector('.cardCvv');
 let submitBtn =document.querySelector('.s-btn');
 const subStatus = document.querySelector('.s-status')
 const inputs =[nameInput, numInput, mm, yy,cvvInput];
+const errorLabel = document.querySelectorAll('small');
+
+const errLabelArr = Array.from(errorLabel)
 
 nameInput.addEventListener('input', (e)=>{
     if(e.target.value.length <= 16){    
         cardName.textContent = e.target.value;
-}
+};
+e.target.value.length > 16 ? e.target.style.borderColor ='red':e.target.style.borderColor ='';
+
+
 })
 numInput.addEventListener('input', (e)=>{
 
@@ -30,29 +36,33 @@ numInput.addEventListener('input', (e)=>{
      e.value = numPattern
     cardNum.textContent = e.value;
 
+    e.target.value.length < 16 ? e.target.style.borderColor ='red':e.target.style.borderColor ='';
+
 })
 mm.addEventListener('input', (e)=>{
     if(e.target.value.length <= 2){  
-        cardMm.textContent = Number(e.target.value);
-        
-} 
+        cardMm.textContent = Number(e.target.value);      
+} ;
+e.target.value.length > 2 ? e.target.style.borderColor ='red':e.target.style.borderColor ='';
+
 })
 yy.addEventListener('input', (e)=>{
     if(e.target.value.length <= 2){    
         cardYy.textContent = Number(e.target.value);
-}
+};
+e.target.value.length > 2 ? e.target.style.borderColor ='red':e.target.style.borderColor ='';
 })
 cvvInput.addEventListener('input', (e)=>{
     if(e.target.value.length <= 3){    
         cardCvv.textContent = Number(e.target.value);
-}
+};
+e.target.value.length > 3? e.target.style.borderColor ='red':e.target.style.borderColor ='';
 })
 
 
 // submit btn
 submitBtn.addEventListener('click', (e)=>{
     e.preventDefault()
-    // const errorLabel = document.querySelectorAll('small');
 
     const isValid = (inputs[0].value.length >=4 || inputs[0].value.length === 16) && inputs[1].value.length === 16 && inputs[2].value.length === 2 && inputs[3].value.length === 2 && inputs[4].value.length === 3 ? 'true': 'false';
 
@@ -63,21 +73,28 @@ submitBtn.addEventListener('click', (e)=>{
     };
 
     if(isValid === 'false'){
-        inputs.filter(e => e.value === '')
-        .forEach(e =>{
-            e.style.color = 'hsl(0, 100%, 66%)';
-            e.style.borderColor='hsl(0, 100%, 66%)'
+        inputs.forEach((e,i) =>{
+            if(e.value === ''){
+                e.style.color = 'hsl(0, 100%, 66%)';
+            e.style.borderColor='hsl(0, 100%, 66%)';
+           errLabelArr[i].textContent = 'this field is required';
+            }
+            else{
+                e.style.color = '';  
+                e.style.borderColor='';
+                errLabelArr[i].textContent = '';
+                
+            }
        return;     
     }) 
 
-   const errorLabel = document.querySelectorAll('small')
-    errorLabel.forEach( e=>{
-        e.textContent =  'this field is required';
-           e.style.color='hsl(0, 100%, 66%)'
-})
-        return;
     };
 
+    inputs[0].value.length > 16? errLabelArr[0].textContent ='Name too long':null;
+    inputs[1].value.length > 0 && inputs[1].value.length < 16? errLabelArr[1].textContent ='enter correct number':null;
+    inputs[2].value.length > 2 ? errLabelArr[2].textContent = 'incorrect month':null;
+    inputs[3].value.length > 2 ? errLabelArr[3].textContent = 'incorrect year':null;
+    inputs[4].value.length > 3 ? errLabelArr[4].textContent ='incorrect':null;
     console.log(isValid);
 
 })
